022b9db75a3422266454f53c330731c913d664f0
Huy Phan <huymini519@gmail.com>
2024-05-06 15:22:48 +0700

    update Cart

--- controller/userController.js
+++ controller/userController.js
@@ -296,8 +296,8 @@ const upDateQuantity = catchAsync(async(req,res,next)=>{
 
 const deleteAllCart = catchAsync(async(req,res,next)=>{
     const user = await User.findById(req.user._id) 
-    user.cart=[];
-    user.save();
+    user.cart={};
+    await user.save();
     res.status(200).json({
         status: 'success',
         message:'Delete cart successfully',
