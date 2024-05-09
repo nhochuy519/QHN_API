fe1fd0bfdc5844408a4e76fa17fddd65be804bf2
Huy Phan <huymini519@gmail.com>
2024-05-09 23:36:09 +0700

    update favorite

--- controller/userController.js
+++ controller/userController.js
@@ -332,7 +332,7 @@ const addFavorites = catchAsync(async (req, res, next) => {
 });
 
 const removeFavorite =  catchAsync(async(req,res,next)=>{
-     await User.findOneAndUpdate(req.user._id,{
+     await User.findByIdAndUpdate(req.user._id,{
         $pull:{
             'favorite':{'products':req.body.productId}
         }
